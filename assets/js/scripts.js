4d96ef1f388e377f9aa03aaa3e62399831266d31
Jorge Solis <jorge.solis.sm@gmail.com>
2024-03-27 00:53:59 -0300

    se añaden arreglos y scripts

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,32 @@
-$(function(){
-    
-$("#searchhero").on("submit", function(event){
-    event.preventDefault();
-    const regexValidacion = /^[a-záéíóúñ0-9\s.]+$/i;
-    let idorname = $("#SuperHero").val();
-    if(regexValidacion.test(idorname)){
-        getSuperhero(idorname);
-    } else {
-        return alert("El número ingresado no es valido");
-    };
+$(function () {
+
+    $("#searchhero").on("submit", function (event) {
+        event.preventDefault();
+        const regexValidacion = /^[0-9]+$/i;
+
+        let idOrName = $("#SuperHero").val();
+
+        if (regexValidacion.test(idOrName)) {
+            //SI ID OR NOMBR ES VÁLIDO
+            getHero(idOrName);
+        } else {
+            return alert("El número ingresado no es valido");
+        };
+    });
+
+    function getHero(idOrName) {
+        let urlBase = "https://superheroapi.com/api/access-token/character-id" + idOrName;
+        $.ajax({
+            method: "GET",
+            url: urlBase,
+            dataType: "json"
+        });
+    }
+
+
+
+
+
 });
 
 
@@ -19,4 +37,3 @@ $("#searchhero").on("submit", function(event){
 
 
 
-});
